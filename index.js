e64c93c1a91338c89a8ab61f689ed770e0c4fc6c
Vinicius Ielo Martins de Oliveira <162700647+viniciusielo4@users.noreply.github.com>
2025-08-20 11:08:15 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
-
-const db = require("./db");
+const db = require("./dbAtual");
 const port = process.env.PORT;
 const express = require('express');
 const app = express();
@@ -13,31 +12,31 @@ app.get('/', (req, res) => {
 });
 
 // Rota para listar um funcionário
-app.get('/funcionarios/:id', async (req, res) => {
+app.get('/cadastrofuncionarios/:id', async (req, res) => {
     const funcionario = await db.selectFuncionario(req.params.id);
     res.json(funcionario);
 });
 
 // Rota para listar todos os funcionários
-app.get('/funcionarios', async (req, res) => {
+app.get('/cadastrofuncionarios', async (req, res) => {
     const funcionarios = await db.selectFuncionarios();
     res.json(funcionarios);
 });
 
 // Rota para inserir funcionário
-app.post('/funcionarios', async (req, res) => {
+app.post('/cadastrofuncionarios', async (req, res) => {
     await db.insertFuncionario(req.body);
     res.sendStatus(201);
 });
 
 // Rota para editar/atualizar funcionário
-app.patch("/funcionarios/:id", async (req, res) => {
+app.patch("/cadastrofuncionarios/:id", async (req, res) => {
     await db.updateFuncionario(req.params.id, req.body);
     res.sendStatus(200);
 });
 
 // Rota para excluir funcionário
-app.delete("/funcionarios/:id", async (req, res) => {
+app.delete("/cadastrofuncionarios/:id", async (req, res) => {
     await db.deleteFuncionario(req.params.id);
     res.sendStatus(204);
 });
